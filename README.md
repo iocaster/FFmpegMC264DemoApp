2b34432f5394b5bf2baa1728ff5af456dca5e943
YongTae Kim <yongtae.yt.kim@gmail.com>
2019-06-05 15:34:21 +0900

    * first commit ...

---
+++ README.md
@@ -0,0 +1,87 @@
+FFmpegMC264Demo
+===============
+
+Use 'mc264','mcaac' instead of 'libx264','aac'.
+
+FFmpegMC264 is a ffmpeg android library powered by MediaCodec HW accelated encoder.<br> 
+Now use 'mc264', 'mcaac' instead of 'libx264','aac' in runing ffmpeg command in your code.
+
+FFmpegMC264 library doesn't call ffmpeg executable file through exec().
+Instead this library has embedded the ffmpeg as an API function.
+So, you can call the ffmpeg like this : 
+* int retcode = mLibFFmpegMC264.Run(cmdString);
+  - cmdString example : 
+  - ffmpeg -i INPUT -vcodec mc264 -acodec mcaac -f mp4 OUTPUT
+  - ffmpeg -i INPUT -vcodec mc264 -b:v 2.0M -r 30 -g 15 -acodec ac3 -f mp4 OUTPUT
+  - ffmpeg -i INPUT -vcodec mc264 -b:v 2.0M -r 30 -g 15 -acodec aac -b:a 64k -f mp4 OUTPUT
+  - - aac : '-b:a 64k' recommended or AV sync problem on my device (LG Q6).
+  - ffmpeg -re -i /sdcard/movie.avi -vcodec mc264 -acodec mcaac -f mpegts udp://192.168.0.12:5555?pkt_size=1316&buffer_size=655360
+  - ffmpeg -i rtsp://192.168.0.10/videodevice -vcodec mc264 -an -f mpegts udp://192.168.0.12:5555?pkt_size=1316
+
+  - generate TV test pattern : 
+  - ffmpeg -f lavfi -i testsrc -pix_fmt yuv420p -vcodec mc264 -b:v 2.0M -r 30 -g 15 -an -f mp4 OUTPUT
+  
+For this, in C side, two encoder modules - mc264.c / mcaac.c - have been added into ffmpeg libavcodec.<br>
+In java side, two encoder controller classes - MC264Encoder.java / MCAACEncoder.java - have been added over android MediaCodec (H.264 / AAC encoders only).
+
+Enjoy ffmpeg powered by MediaCodec HW accelated encoder.
+
+
+## The APIs you have to know : 
+
+### prepare an instance
+* mLibFFmpegMC264 = new LibFFmpegMC264();
+* mLibFFmpegMC264.getMC264Encoder().setYUVFrameListener(this, true);  - optional API
+
+### prepare an AsyncTask
+* ffmpeg_task = new MyTask(this);
+* ffmpeg_task.execute( ffmpegCmdStringArray );
+
+### run the instance in the task :: doInBackground()
+* mLibFFmpegMC264.Ready();
+* mLibFFmpegMC264.Run( strings );
+
+### stop
+* mLibFFmpegMC264.Stop();
+* mLibFFmpegMC264.ForceStop(); - optional API to enforce stop
+
+### post stop
+* mLibFFmpegMC264.Reset();
+
+
+## Supported Color Format :
+* ffmpeg INPUT stream : yuv420p
+* MediaCodec : YV12 [NV12] (MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420SemiPlanar)
+
+The INPUT stream should have the color format YUV420Planar. Or use '-pix_fmt yuv420p' option with INPUT.<br>
+The MediaCodec of your device should have the color format YV12 [NV12]. (Other formats will be supported later)
+
+## Limitations :
+* The running time is limited to 30 minutes. Contact me for the unlimited version.
+* The video bitrate is limited to 2Mbps.
+
+
+## Screenshot
+<p align="center">
+  <img src="./FFmpegMC264Demo-Screen.png" width="350" height="720">
+</p>
+
+## Referenced Links :
+* MediaCodec example :
+  - https://android.googlesource.com/platform/cts/+/jb-mr2-release/tests/tests/media/src/android/media/cts/EncodeDecodeTest.java
+* Handling H.264 SPS / PPS :
+  - https://github.com/Kickflip/kickflip-android-sdk/blob/master/sdk/src/main/java/io/kickflip/sdk/av/FFmpegMuxer.java
+* https://stackoverflow.com/questions/24884827/possible-locations-for-sequence-picture-parameter-sets-for-h-264-stream/24890903#24890903
+* https://stackoverflow.com/questions/20909252/calculate-pts-before-frame-encoding-in-ffmpeg
+* http://leo.ugr.es/elvira/devel/Tutorial/Java/native1.1/implementing/index.html
+* Color Format :
+  - https://software.intel.com/en-us/ipp-dev-reference-pixel-and-planar-image-formats#FIG6-15
+  - https://developer.android.com/reference/android/graphics/ImageFormat.html#YV12
+  - https://en.wikipedia.org/wiki/YUV
+* VideoKit :
+  - https://github.com/inFullMobile/videokit-ffmpeg-android
+  - https://github.com/IljaKosynkin/FFmpeg-Development-Kit
+* Cross Compiling FFmpeg 4.0 for Android :
+  - https://medium.com/@karthikcodes1999/cross-compiling-ffmpeg-4-0-for-android-b988326f16f2
+
+
---
+++ app/src/main/java/com/iocaster/ffmpegmc264demoapp/MainActivity.java
@@ -0,0 +1,211 @@
+package com.iocaster.ffmpegmc264demoapp;
+
+import android.Manifest;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+
+import kim.yt.ffmpegmc264.LibFFmpegMC264;
+import kim.yt.ffmpegmc264.MC264Encoder;
+
+
+public class MainActivity extends AppCompatActivity
+        implements MC264Encoder.YUVFrameListener
+{
+    private final static String TAG = "MainActivity";
+    EditText urlSrc;
+    private View spinnerContainer;
+    private static boolean spinnerActive = false;
+
+    private MyTask ffmpeg_task = null;
+    private static LibFFmpegMC264 mLibFFmpeg;
+    private static int ffmpeg_retcode = 0;
+
+    /*
+     * To draw a progress monitor view with YUVFrame
+     */
+    private static ImageView monitorView;
+    private static ArrayList mFrameList = new ArrayList();
+    private static int mWidth, mHeight;
+    private static Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (msg.what == 1){
+                if( ! mFrameList.isEmpty() ) {
+                    byte[] frameData = (byte[]) mFrameList.get(0);
+                    drawYUVFrame( frameData, mWidth, mHeight );
+                    mFrameList.remove(0);
+                }
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_new);
+
+        /*
+         * request an android permission : WRITE_EXTERNAL_STORAGE
+         * It is required when to save the ffmpeg output into a file.
+         */
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                1337);
+
+        mLibFFmpeg = new LibFFmpegMC264();
+        mLibFFmpeg.getMC264Encoder().setYUVFrameListener(this, true);
+
+        EditText usagelabel = findViewById(R.id.usageLabel);
+        //usagelabel.setFocusable(false);         //show normally but not editable
+        usagelabel.setEnabled(false);        //show as gray
+
+        EditText usagebody = findViewById(R.id.usageBody);
+        //usagebody.setFocusable(false);     //show normally but not editable
+        usagebody.setEnabled(false);        //show as gray
+
+        EditText usagebody2 = findViewById(R.id.usageBody2);
+        //usagebody2.setFocusable(false);     //show normally but not editable
+        usagebody2.setEnabled(false);        //show as gray
+
+        urlSrc = findViewById(R.id.editText4url);
+        Button process = findViewById(R.id.btn_process);
+        Button btn_stop = findViewById(R.id.btn_stop);
+        /*ImageView*/ monitorView = findViewById(R.id.imageView);
+
+        String defaultCmd = getResources().getString(R.string.ffmpeg_cmd_pc1);
+        String savedCmd = PreferenceManager.getDefaultSharedPreferences(this).getString("cmd", defaultCmd);
+        if( savedCmd != null && savedCmd.length() > 6 ) // 6 == sizeof("ffmpeg");
+            urlSrc.setText( savedCmd );
+        else {
+            urlSrc.setText(defaultCmd);
+            saveCmd(defaultCmd);
+        }
+
+        spinnerContainer = findViewById(R.id.v_spinner_container);
+        spinnerContainer.setVisibility(View.VISIBLE);
+
+//moved into layout xml file - activity_main_new.xml
+//        process.setOnClickListener(this::processClicked);
+//        btn_stop.setOnClickListener(this::btnStopClicked);
+
+        spinnerContainer.setVisibility(View.GONE);
+    }
+
+    private void saveCmd( String cmdStr ) {
+        //String[] sArrays = cmdStr.split("\\s+");   //+ : to remove duplicate whitespace
+        PreferenceManager.getDefaultSharedPreferences(this).edit().putString("cmd", cmdStr ).apply();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        if( !spinnerActive )
+            spinnerContainer.setVisibility(View.GONE);
+        else
+            spinnerContainer.setVisibility(View.VISIBLE);
+    }
+
+    public void processClicked(View ignored) {
+        Toast.makeText(this, "Start Clicked !!!", Toast.LENGTH_SHORT).show();
+
+        spinnerContainer.setVisibility(View.VISIBLE);
+        spinnerActive = true;
+
+        String fullUrl = new String("") + urlSrc.getText();
+        String[] sArrays = fullUrl.split("\\s+");   //+ : to remove duplicate whitespace
+        saveCmd(fullUrl);
+
+        ffmpeg_task = new MyTask(this);
+        ffmpeg_task.execute( sArrays );
+    }
+
+    private int stopCnt = 0;
+    public void btnStopClicked(View ignored) {
+        Toast.makeText(this, "Stop Clicked !!!", Toast.LENGTH_SHORT).show();
+
+        String fullUrl = new String("") + urlSrc.getText();
+        saveCmd(fullUrl);
+
+        mLibFFmpeg.Stop();
+        //mLibFFmpeg.Reset();         //Please, call mLibFFmpeg.Reset() inside onPostExecute() not here
+
+        if( ++stopCnt >= 3 )
+            mLibFFmpeg.ForceStop();
+    }
+
+    void finished() {
+        Toast.makeText(this, "ffmpeg finished !!!  (retcode = " + ffmpeg_retcode + ")", Toast.LENGTH_SHORT).show();
+        spinnerContainer.setVisibility(View.GONE);
+        spinnerActive = false;
+
+        ffmpeg_task = null;
+        ffmpeg_retcode = 0;
+        stopCnt = 0;
+    }
+
+    private static class MyTask extends AsyncTask<String, Void, Void> {
+        private final WeakReference<MainActivity> activityWeakReference;
+
+        MyTask(MainActivity activity) {
+            activityWeakReference = new WeakReference<>(activity);
+        }
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            mLibFFmpeg.Ready();
+            ffmpeg_retcode = mLibFFmpeg.Run(strings);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            mLibFFmpeg.Reset();
+            final MainActivity activity = activityWeakReference.get();
+            if (activity != null) {
+                activity.finished();
+            }
+        }
+    }
+
+    @Override
+    public void onYUVFrame(byte[] frameData, int width, int height) {
+        mFrameList.add( frameData );
+        mWidth = width;
+        mHeight = height;
+
+        Message msg = new Message();
+        msg.what = 1;
+        mHandler.sendMessage( msg );
+    }
+
+    public static void drawYUVFrame(byte[] frameData, int width, int height) {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        YuvImage yuvImage = new YuvImage(frameData, ImageFormat.NV21, width, height, null);
+        yuvImage.compressToJpeg(new Rect(0, 0, width, height), 50, out);
+        byte[] imageBytes = out.toByteArray();
+        Bitmap image = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+        monitorView.setImageBitmap(image);
+    }
+}
